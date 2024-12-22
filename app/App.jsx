54536d9cfd439a4dc714eb05585d36191ce860fe
Suhas Nidgundi <suhasnidgundi@gmail.com>
2024-12-22 12:58:55 +0530

    feat: add animate.css for enhanced animations in the application

--- app/App.jsx
+++ app/App.jsx
@@ -2,6 +2,7 @@ import { RouterProvider } from 'react-router-dom';
 import { useEffect } from 'react';
 import useAuthStore from './store/authStore';
 import { router } from './routes/AppRoutes';
+import 'animate.css';
 
 function App() {
   const { checkAuthStatus } = useAuthStore();
